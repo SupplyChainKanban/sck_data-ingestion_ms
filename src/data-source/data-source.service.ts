d00419800b2b296112eb5f2a6d16a0c6650c6a53
Slater <armando.jaimes@pucp.edu.pe>
2024-09-14 20:49:15 -0500

    Agregar manejo de errores en los servicios

--- src/data-source/data-source.service.ts
+++ src/data-source/data-source.service.ts
@@ -6,7 +6,7 @@ import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class DataSourceService extends PrismaClient implements OnModuleInit {
-  private readonly logger = new Logger('IngestionService');
+  private readonly logger = new Logger('DataSourceService');
 
   onModuleInit() {
     this.$connect();
@@ -14,69 +14,84 @@ export class DataSourceService extends PrismaClient implements OnModuleInit {
   }
 
   create(createDataSourceDto: CreateDataSourceDto) {
-    return this.dataSource.create({
-      data: createDataSourceDto
-    });
+    try {
+      return this.dataSource.create({ data: createDataSourceDto });
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   async findAll(paginationDto: PaginationDto) {
     const { page, limit } = paginationDto;
-    const totalRecords = await this.dataSource.count({ where: { available: true } });
-    const lastPage = Math.ceil(totalRecords / limit)
-    const data = await this.dataSource.findMany({
-      skip: (page - 1) * limit,
-      take: limit,
-      where: {
-        available: true,
-      }
-    })
 
-    return {
-      data,
-      meta: {
-        page,
-        totalRecords,
-        lastPage,
+    try {
+      const totalRecords = await this.dataSource.count({ where: { available: true } });
+      const lastPage = Math.ceil(totalRecords / limit)
+      const data = await this.dataSource.findMany({
+        skip: (page - 1) * limit, take: limit, where: { available: true }
+      })
+
+      return {
+        data, meta: { page, totalRecords, lastPage }
       }
+    } catch (error) {
+      this.handleExceptions(error);
     }
   }
 
   async findOne(id: number) {
-    const dataSource = await this.dataSource.findFirst({
-      where: { id: id, available: true }
-    })
-    if (!dataSource) {
-      throw new RpcException({
-        message: `Data Source with id #${id} not found`,
-        status: HttpStatus.BAD_REQUEST
+    try {
+      const dataSource = await this.dataSource.findFirst({
+        where: { id: id, available: true }
       })
+      if (!dataSource) {
+        throw new RpcException({
+          message: `Data Source with id ${id} not found`,
+          status: HttpStatus.NOT_FOUND,
+        })
+      }
+
+      return dataSource;
+    } catch (error) {
+      this.handleExceptions(error);
     }
-    return dataSource;
   }
 
   async update(id: number, updateDataSourceDto: UpdateDataSourceDto) {
+    //TODO: Falta colocar el dto para que se actualize
     //? Aquí se colocará el id solo para que funcione el TCP
     const { id: __, ...data } = updateDataSourceDto;
-
     await this.findOne(id);
 
-    return this.dataSource.update({
-      where: { id: id },
-      //? data: updateDataSourceDto,
-      data: data,
-    })
+    try {
+      return this.dataSource.update({
+        where: { id: id },
+        //? data: updateDataSourceDto,
+        data: data,
+      })
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   async remove(id: number) {
     await this.findOne(id);
+    try {
+      const dataSource = await this.dataSource.update({ where: { id }, data: { available: false } })
 
-    const dataSource = await this.dataSource.update({
-      where: { id },
-      data: {
-        available: false
-      }
-    })
+      return dataSource;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
 
-    return dataSource;
+  private handleExceptions(error: any) {
+    switch (error.code) {
+      case '123123123123':
+        throw new RpcException({ status: HttpStatus.I_AM_A_TEAPOT, message: 'Error manejado' });
+      default:
+        this.logger.error(error);
+        throw new RpcException({ status: HttpStatus.BAD_REQUEST, message: error.message });
+    }
   }
 }
--- src/raw-data/raw-data.service.ts
+++ src/raw-data/raw-data.service.ts
@@ -7,7 +7,7 @@ import { PaginationDto } from 'src/common';
 @Injectable()
 export class RawDataService extends PrismaClient implements OnModuleInit {
 
-  private readonly logger = new Logger('IngestionService');
+  private readonly logger = new Logger('RawDataService');
 
   onModuleInit() {
     this.$connect();
@@ -15,70 +15,69 @@ export class RawDataService extends PrismaClient implements OnModuleInit {
   }
 
   create(createRawDataDto: CreateRawDataDto) {
-    return this.rawData.create({
-      data: createRawDataDto
-    })
+    try {
+      return this.rawData.create({ data: createRawDataDto })
+    } catch (error) {
+      this.logger.error(error)
+      throw new RpcException({ status: HttpStatus.BAD_REQUEST, message: error.message })
+    }
   }
 
   async findAll(paginationDto: PaginationDto) {
     const { page, limit } = paginationDto;
-    const totalRecords = await this.rawData.count({ where: { available: true } });
-    const lastPage = Math.ceil(totalRecords / limit)
-    const data = await this.rawData.findMany({
-      skip: (page - 1) * limit,
-      take: limit,
-      where: {
-        available: true,
-      }
-    })
 
-    return {
-      data,
-      meta: {
-        page,
-        totalRecords,
-        lastPage
-      }
-    };
+    try {
+      const totalRecords = await this.rawData.count({ where: { available: true } });
+      const lastPage = Math.ceil(totalRecords / limit)
+      const data = await this.rawData.findMany({ skip: (page - 1) * limit, take: limit, where: { available: true } })
+
+      return { data, meta: { page, totalRecords, lastPage } };
+    } catch (error) {
+      this.logger.error(error)
+      throw new RpcException({ status: HttpStatus, message: error.message })
+    }
   }
 
   async findOne(id: number) {
-    const rawData = await this.rawData.findFirst({
-      where: { id: id, available: true }
-    })
-    if (!rawData) {
-      throw new RpcException({
-        message: `Raw Data with id #${id} not found`,
-        status: HttpStatus.BAD_REQUEST
-      })
-    }
+    try {
+      const rawData = await this.rawData.findFirst({ where: { id: id, available: true } })
+      if (!rawData) {
+        throw new RpcException({ message: `Raw Data with id ${id} not found`, status: HttpStatus.BAD_REQUEST })
+      }
 
-    return rawData;
+      return rawData;
+    } catch (error) {
+      this.logger.error(error)
+      throw new RpcException({ status: HttpStatus, message: error.message })
+    }
   }
 
   async update(id: number, updateRawDataDto: UpdateRawDataDto) {
     //? Aquí se colocará el id solo para que funcione el TCP
     const { id: __, ...data } = updateRawDataDto;
-
     await this.findOne(id);
 
-    return this.rawData.update({
-      where: { id: id },
-      //? data: updateRawDataDto,
-      data: data,
-    })
+    try {
+      return this.rawData.update({
+        where: { id: id },
+        //? data: updateRawDataDto,
+        data: data,
+      })
+    } catch (error) {
+      this.logger.error(error)
+      throw new RpcException({ status: HttpStatus, message: error.message })
+    }
   }
 
   async remove(id: number) {
     await this.findOne(id);
+    try {
+      const rawData = await this.rawData.update({ where: { id }, data: { available: false } })
 
-    const rawData = await this.rawData.update({
-      where: { id },
-      data: {
-        available: false
-      }
-    })
-
-    return rawData
+      return rawData
+    } catch (error) {
+      this.logger.error(error)
+      throw new RpcException({ status: HttpStatus, message: error.message })
+    }
   }
 }
